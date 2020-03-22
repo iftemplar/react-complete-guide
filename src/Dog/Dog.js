5305dd79a5426b4a6c315c0581181ca79c47525b
iftemplar <terry04777@gmail.com>
2020-03-22 23:29:12 +0200

    35 Put logic into JSX

--- src/Dog/Dog.js
+++ src/Dog/Dog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dog = () => {
-    return <div>Rumba the dog</div>
+return <div>Rumba the dog. She is {Math.floor(Math.random()*20)} years old</div>
 }
 
 export default Dog;
