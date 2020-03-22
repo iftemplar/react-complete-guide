7ffa67e2598796cc3eecdebdf7420e6eb0c7763b
iftemplar <terry04777@gmail.com>
2020-03-22 17:39:25 +0200

    30-JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hello</h1>
+    //   </div>
+    // );
+    return React.createElement('section', {className: 'App rumba'}, React.createElement('h1', null, 'The dog'))
   }
 }
 
