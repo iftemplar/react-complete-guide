2ca1d215ae1d1ef662a15ad3274498114252e0a5
Mykola Samila <mykola.samila@enverus.com>
2020-06-26 21:00:17 +0300

    State property used to manage App.js data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Dog from './Dog/Dog';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-        <Dog name='Rumba' age='4'>She likes to walk three times a day</Dog>
-        <Dog name='Mishka' age='1' />
-        <Dog name='Niusha' age='1' />
-      </div>
-    );
+    // state  is a property of the component class
+    // if state changes it leads react to re-render the changed part
+    state = {
+        doggos: [
+            {name: 'Rumba', age: 4},
+            {name: 'Mishka', age: 2},
+            {name: 'Niusha', age: 1}
+            ]
+    }
+
+      render() {
+        return (
+          <div className="App">
+            <h1>Hello</h1>
+            <Dog name={this.state.doggos[0].name} age={this.state.doggos[0].age}>She likes to walk three times a day</Dog>
+            <Dog name={this.state.doggos[1].name} age={this.state.doggos[1].age} />
+            <Dog name={this.state.doggos[2].name} age={this.state.doggos[2].age} />
+          </div>
+        );
     // return React.createElement('section', {className: 'App rumba'}, React.createElement('h1', null, 'The dog'))
   }
 }
