3effdac70b32abde8b43cc3ceaabffc422a1ab5e
iftemplar <terry04777@gmail.com>
2020-06-28 14:41:09 +0300

    Basic styling for Dog cards

--- src/Dog/Dog.js
+++ src/Dog/Dog.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Dog.css';
 
 const Dog = (props) => {
 return (
-    <section>
+    <section className='Dog'>
         <div onClick={props.click}>{props.name} the dog. The dog is {props.age} years old.
             <span>{props.children}</span>
             <input type="text" onChange={props.changed} value={props.name}/>
